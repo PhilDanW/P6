c2a28a0321f83cbe15bf1cb393739379504498a5
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-05-04 20:34:50 -0500

    Update header.h

--- header.h
+++ header.h
@@ -1,134 +1,69 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef _SHM_HEADER_H
+#define _SHM_HEADER_H
 
-#include <stdio.h>
-#include <sys/shm.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <sys/msg.h>
-#include <unistd.h>
-#include <string.h>
-#include <signal.h>
-#include <time.h>
+// #define SHM_KEY 19942017
+#define MAX_BUF_SIZE 1024
 
-#define MAX_PROCS 18
-#define SHMKEY 123123
+#define DEBUG 0
+#define NANO_MOD 1000000000
+#define FRAME_SIZE 1024
 
-// #####
-// ##### STRUCTURES #####
-// #####
+typedef struct 
+{
+	long long seconds;
+	long long nanoseconds;
+	char shmMsg[MAX_BUF_SIZE];
+	int sigNotReceived;
+} shared_oss_struct;
 
-// struct for time
+typedef struct 
+{
+	long long seconds;
+	long long nanoseconds;
+	pid_t procID;
+	int pageNumRequested;
+	int readwrite;
+} shmMsg;
+
 typedef struct {
-    unsigned int seconds;
-    unsigned int milliseconds;
-} systemClock_t;
+	int pageID;
+	int dirty;
+	int unused;
+	int valid;
+	int second_chance;
+	int frameContent[FRAME_SIZE];
+} Page;
 
-// frame table
 typedef struct {
-    int frames[256];
-    int PIDS[256];
-    int referenceFlag[256];
-    char dirtyBit[256];
-} frameTable_t;
+	Page page;
+	int isFrameAssigned;
+	int referenceBit;
+} Frame;
 
-// page table
 typedef struct {
-    int pages[32];
-} pageTable_t;
+	Frame frames[256];
+	int frameID;
+	int delimiter;
+} PageTable;
 
-//for shared memory
-typedef struct {
-    int sharedPIDHolder[18];         // for checking program termination
-    int checkProcNum[18];
-    int processAddressCalled[18];
-    int processReadOrWrite[18];
-    int processCallCount[18];
-} shared_t;
+typedef struct msgbuf {
+  long mType;
+  char mText[80];
+} msgbuf;
 
-struct mesg_buffer {
-    long mesg_type;
-    char mesg_text[100];
-} message;
+// queue
+struct node
+{
+    int pageNum;
+    struct node *ptr;
+}*front,*rear,*temp,*front1;
 
-// #####
-// ##### VARIABLES #####
-// #####
+int frontelement();
+void enq(int data);
+int deq();
+void empty();
+void display();
+void create();
+void queuesize();
 
-frameTable_t frameTable;        // define frame table
-pageTable_t pageTable[MAX_PROCS];   // define array of page tables
-systemClock_t theClock;         // define clock
-int sharedShmid;                // shmem - holds the shared memory segment id
-shared_t *sharedShmptr;            // shmem - points to the data structure
-
-int msgid;                      // for message queue
-key_t key;                      // for message queue
-
-int proccessesRunning = 0;      // variable that terminates program
-
-int mainPIDHolder[18] = {};    //the main pid holder user to check for termination
-int numForksMade = 0;
-int randTimeToFork[18];         // each processes random time to fork
-
-int procsRunning = 0;
-
-int numWritesToLog = 0;
-
-int PID;        // holds a temp PID
-
-// when flagged 1, program will terminate
-// #####
-// ##### FUNCTIONS #####
-// #####
-
-void sharedMemoryConfig() {
-    //shared mem for sysClock
-
-    sharedShmid = shmget(SHMKEY, sizeof(systemClock_t), IPC_CREAT|0777);
-    if(sharedShmid < 0)
-    {
-        perror("sysClock shmget error in master \n");
-        exit(errno);
-    }
-    sharedShmptr = shmat(sharedShmid, NULL, 0);
-    if(sharedShmptr < 0){
-        perror("sysClock shmat error in oss\n");
-        exit(errno);
-    }
-}
-
-void messageQueueConfig(){
-        // ftok to generate unique key
-        key = ftok("progfile", 65);
-        // msgget creates a message queue
-        // and returns identifier
-        msgid = msgget(key, 0666 | IPC_CREAT);
-}
-
-void initRandomForkTimes(){
-
-    // init clock to random
-    time_t t;
-    srand((unsigned) time(&t));
-
-    int ii;
-    for(ii = 0; ii < 18; ii++){
-        randTimeToFork[ii] = (rand() % (500 - 100)) + 100;
-    }
-
-}
-
-#endif //ASS6_HEADER_H
-
-
-
-
-
-//// adjust request timer
-//if ((nanoseconds + timeIncrement) > 999999999){
-//rollover = (nanoseconds + timeIncrement) - 999999999;
-//seconds += 1;
-//nanoseconds = rollover;
-//} else {
-//nanoseconds += timeIncrement;
-//}
+#endif
